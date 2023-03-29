52b7fa8ce04303f0b149ad961979e3a96cfd257d
Christian Garcia <bones192nd@gmail.com>
2023-03-29 15:47:30 -0400

    partial lab, will work on it at home

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,13 @@ function App() {
   const [items, setItems] = useState(itemData);
   const [isDarkMode, setIsDarkMode] = useState(false);
 
+
+  // function for onChange
+  
+  // function for onItemFormSubmit
+
+
+
   function handleDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
 function ItemForm(props) {
+
+  const [formData, setFormData]  = useState (
+    { 
+      name : "", 
+      category: "Produce"
+  })
+
+
+
+  function handleChange(event){
+    const name = event.target.name
+    const value = event.target.value
+
+    setFormData({...formData, [name] : value})
+  }
+
+  function onItemFormSubmit(event){
+    event.preventDefault()
+    
+
+    console.log(formData)
+  }
+
   return (
-    <form className="NewItem">
+    <form 
+        className="NewItem"
+        onSubmit={onItemFormSubmit}
+    >
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" onChange = {handleChange}/>
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" onChange = {handleChange}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
